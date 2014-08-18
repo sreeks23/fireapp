37a6f91efa86bd90e70e5ed29bbd7ee7d620e763
sreeks23 <Sreeks@Sreekanths-MacBook-Pro.local>
2014-08-17 23:22:29 -0700

    fireapp
    sunday eod

--- gen/com/ibm/fireapp/R.java
+++ gen/com/ibm/fireapp/R.java
@@ -1096,14 +1096,21 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f050041;
+        public static final int action_settings=0x7f050048;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int bcis=0x7f05003c;
+        public static final int bclaims=0x7f050042;
+        public static final int bcpi=0x7f05003e;
+        public static final int bcws=0x7f050043;
         public static final int beginning=0x7f050011;
-        public static final int benter=0x7f05003f;
+        public static final int benter=0x7f050046;
+        public static final int bepas=0x7f05003f;
+        public static final int bepic=0x7f05003d;
+        public static final int bhoact=0x7f050041;
+        public static final int bpiauto=0x7f050040;
         public static final int checkbox=0x7f05002d;
-        public static final int cislv1=0x7f05003d;
+        public static final int cislv1=0x7f050044;
         public static final int collapseActionView=0x7f05000d;
         public static final int default_activity_button=0x7f050028;
         public static final int dialog=0x7f05000e;
@@ -1124,11 +1131,11 @@ containing a value of this type.
         public static final int never=0x7f050009;
         public static final int none=0x7f050010;
         public static final int normal=0x7f050000;
-        public static final int para1=0x7f05003e;
+        public static final int para1=0x7f050045;
         public static final int progress_circular=0x7f050018;
         public static final int progress_horizontal=0x7f050019;
         public static final int radio=0x7f05002f;
-        public static final int rowTextView=0x7f050040;
+        public static final int rowTextView=0x7f050047;
         public static final int search_badge=0x7f050032;
         public static final int search_bar=0x7f050031;
         public static final int search_button=0x7f050033;
@@ -1209,8 +1216,9 @@ containing a value of this type.
         public static final int activity_cis_rating=0x7f03001c;
         public static final int activity_home=0x7f03001d;
         public static final int activity_logo=0x7f03001e;
-        public static final int simplerow=0x7f03001f;
-        public static final int support_simple_spinner_dropdown_item=0x7f030020;
+        public static final int activity_uc=0x7f03001f;
+        public static final int simplerow=0x7f030020;
+        public static final int support_simple_spinner_dropdown_item=0x7f030021;
     }
     public static final class menu {
         public static final int logo=0x7f0c0000;
